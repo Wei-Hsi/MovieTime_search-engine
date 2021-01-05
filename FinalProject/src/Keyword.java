41c98b0bb3f464b4ccb590912d93c83e37189308
FrankHsu <105202040@mail2.nccu.tw>
2021-01-05 16:19:10 +0800

    first commit

---
+++ FinalProject/src/Keyword.java
@@ -0,0 +1,25 @@
+
+
+public class Keyword {
+	public String name;
+	public float weight;
+
+	public Keyword(String name, float weight) {
+		this.name = name;
+		this.weight = weight;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + name + "," + weight + "]";
+	}
+
+}
